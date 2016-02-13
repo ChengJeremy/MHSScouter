b43d1397e60375e5737bf6c69964e68253dedc4b
Jeremy Cheng <jyz.cheng@gmail.com>
2016-02-13 08:55:23 -0500

    MHSScouter
    Created project and added xml outlines

---
+++ app/src/main/java/com/example/jerem/mhsscouter/MHSScouter.java
@@ -0,0 +1,23 @@
+package com.example.jerem.mhsscouter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MHSScouter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mhsscouter);
+    }
+
+    protected void clicked(){
+        //change class to an input class with database and relearn all that
+        //allow to take images of robots if necessary???
+        //ability to link with online web database - consult with Abhinav
+        //offline mode???
+        //ability to edit rows and columns also according to online database... make everything local for now though
+        //change app logo widget design - it cannot be just an android robot :P
+        //why does the window get messed up when I turn it horizontal? Shouldn't everything be relative to each other? Look into this later.
+    }
+}
